49ed3883b8e807e36e7a1aeba867d784c6afa54f
LiuShihao <1692312138@qq.com>
2022-02-01 14:01:09 +0800

    1.找出有序数组中大于等于num最左的位置 2.找出有序数组中小于等于num最右的位置

--- src/main/java/com/lsh/day04/Code01.java
+++ src/main/java/com/lsh/day04/Code01.java
@@ -3,7 +3,7 @@ package com.lsh.day04;
 /**
  * @author ：LiuShihao
  * @date ：Created in 2022/1/31 1:37 下午
- * @desc ：
+ * @desc ：二分法：找出有序数组中的num
  */
 public class Code01 {
 
@@ -12,33 +12,40 @@ public class Code01 {
         for (int i = 0; i < timetest; i++) {
             int[] arr = generateRandomArray(10, 20);
             bubbleSort(arr);
-            if (find(arr,5)){
-                //find方法没有找到
-                if (test(arr,5)){
-                    printArr(arr);
-                    System.out.println("找到了");
-                    System.out.println("==========");
-                }else{
-                    printArr(arr);
-                    System.out.println("错误！");
-                    System.out.println("==========");
-                }
-            }
+//            if (find(arr,5)){
+//                //find方法没有找到
+//                if (test(arr,5)){
+//                    printArr(arr);
+//                    System.out.println("找到了");
+//                    System.out.println("==========");
+//                }else{
+//                    printArr(arr);
+//                    System.out.println("错误！");
+//                    System.out.println("==========");
+//                }
+//            }
+            printArr(arr);
+
+//            System.out.println(mostLeftNoLessIndex(arr,5));
+//            System.out.println(test2(arr,5));
+
+            System.out.println(mostRightNoLessThenIndex(arr,5));
+            System.out.println(test3(arr,5));
+
+            System.out.println("======================");
+
+
+
+
         }
 
-//        int[] arr = generateRandomArray(10, 10);
-//        printArr(arr);
-//        bubbleSort(arr);
-//        printArr(arr);
-//        System.out.println(test(arr, 5));
-//        System.out.println(find(arr, 5));
 
         System.out.println("==完成==");
 
     }
 
     /**
-     * 通过二分法查找数组中的num
+     * 题目一：通过二分法查找数组中的num
      * @param arr
      * @param num
      */
@@ -60,6 +67,58 @@ public class Code01 {
     }
 
     /**
+     * 升级2：找出数组中大于等于num 的最左边的数
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static int mostLeftNoLessIndex(int[] arr,int num){
+        if (arr == null || arr.length == 0 ){
+            return  -1;
+        }
+        int L = 0 ;
+        int R = arr.length-1;
+        int ans = -1;
+        while (L <= R){
+            int mid = (L+R)/2;
+            if (arr[mid] >= num){
+                ans = mid;
+                R = mid - 1;
+            }else {
+                //arr[mid] < num
+                L = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 升级3 ：在有序数组arr中 小于等于num最右边的数
+     * @param arr   2，4，6，5，5，8，9
+     * @param num
+     * @return
+     */
+    public static int mostRightNoLessThenIndex(int[] arr,int num ){
+        if (arr == null || arr.length == 0){
+            return  -1;
+        }
+        int L= 0;
+        int R= arr.length-1;
+        int ans = -1;
+        while (L<= R){
+            int mid = (L+R)/2;
+            if (arr[mid] <= num){
+                ans = mid;
+                L = mid + 1;
+            }else {
+                //arr[mid] > num
+                R = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    /**
      * 对数器：测试find 方法是否正确
      * @param arr
      * @param num
@@ -72,6 +131,38 @@ public class Code01 {
             }
         }
         return false;
+    }
+
+    /**
+     * 对数器2： 测试mostLeftNoLessIndex方法是否正确
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static int test2(int[] arr,int num){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= num){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    /**
+     * 对数器3 测试方法
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static int test3(int[] arr,int num){
+        int ans = -1;
+        for (int i = arr.length-1; i >= 0  ; i--) {
+            if (arr[i] <= num){
+                return i;
+            }
+        }
+        return ans;
     }
 
 
