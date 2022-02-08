5324602a4a67e7d4ed38353fb03062fcf79b2fd4
LiuShihao <1692312138@qq.com>
2022-02-08 09:31:43 +0800

    位图的实现

--- src/main/java/com/lsh/day06/Code01_BitMap.java
+++ src/main/java/com/lsh/day06/Code01_BitMap.java
@@ -1,5 +1,7 @@
 package com.lsh.day06;
 
+import java.util.HashSet;
+
 /**
  * @author ：LiuShihao
  * @date ：Created in 2022/2/7 4:36 下午
@@ -11,14 +13,32 @@ package com.lsh.day06;
  */
 public class Code01_BitMap {
 
-    public static class BigMap{
+    public static class BitMap{
         private long[] bits;
 
-        public BigMap(int max){
+        public BitMap(int max){
             // >> 6表示 ➗64
             //(max + 64) >> 6   等同于  (max + 64)/64
             bits = new long[(max + 64) >> 6];
         }
+
+        /**
+         * 要用1L左移 表示64位
+         * 用1 则为int类型 ，只有32位
+         * @param num
+         */
+        public void add(int num){
+            bits[num >> 6] |= (1L << (num & 63));
+        }
+        public void delete(int num){
+            bits[num >> 6] &= ~(1L << (num & 63));
+        }
+        public boolean contains(int num){
+            return (bits[num >> 6] & (1L << (num & 63))) != 0;
+        }
+
+
+
     }
 
     /**
@@ -27,10 +47,37 @@ public class Code01_BitMap {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(8363>>6);
-        System.out.println(8363/64);
-        System.out.println(8363%64);
-        System.out.println(8363&63);
+//        System.out.println(8363>>6);
+//        System.out.println(8363/64);
+//        System.out.println(8363%64);
+//        System.out.println(8363&63);
+        System.out.println("测试开始！");
+        int max = 10000;
+        BitMap bitMap = new BitMap(max);
+        HashSet<Integer> set = new HashSet<>();
+        int testTime = 10000000;
+        for (int i = 0; i < testTime; i++) {
+            int num = (int) (Math.random() * (max + 1));
+            double decide = Math.random();
+            if (decide < 0.333) {
+                bitMap.add(num);
+                set.add(num);
+            } else if (decide < 0.666) {
+                bitMap.delete(num);
+                set.remove(num);
+            } else {
+                if (bitMap.contains(num) != set.contains(num)) {
+                    System.out.println("Oops!");
+                    break;
+                }
+            }
+        }
+        for (int num = 0; num <= max; num++) {
+            if (bitMap.contains(num) != set.contains(num)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("测试结束！");
 
     }
 
