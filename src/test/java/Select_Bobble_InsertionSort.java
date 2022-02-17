864bcb8be7bfb98d58450ac1e8b8a353538e2874
LiuShihao <1692312138@qq.com>
2022-02-17 14:16:24 +0800

    选择排序、冒泡排序、插入排序、对数器

--- src/test/java/Select_Bobble_InsertionSort.java
+++ src/test/java/Select_Bobble_InsertionSort.java
@@ -7,6 +7,10 @@ import java.util.Arrays;
  */
 public class Select_Bobble_InsertionSort {
 
+    /**
+     * 遍历打印数组元素
+     * @param arr
+     */
     public static void printArr(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             System.out.print( arr[i] +" ");
@@ -14,6 +18,12 @@ public class Select_Bobble_InsertionSort {
         System.out.println();
     }
 
+    /**
+     * 随机生成一个数组
+     * @param maxLength 数组最大长度
+     * @param maxValue 数组中元素最大值
+     * @return
+     */
     public static int[] generateRandomArr(int maxLength,int maxValue){
         maxLength = (int)(Math.random() * maxLength+1);
         int[] arr = new int[maxLength];
@@ -22,6 +32,12 @@ public class Select_Bobble_InsertionSort {
         }
         return arr;
     }
+
+    /**
+     * 复制数组
+     * @param arr
+     * @return 返回一个新的相同的数组
+     */
     public static int[] copy(int[] arr){
         int[] newArr = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
@@ -30,12 +46,23 @@ public class Select_Bobble_InsertionSort {
         return newArr;
     }
 
+    /**
+     * 交换数组中两个位置的数
+     * @param arr
+     * @param i
+     * @param j
+     */
     public static void swap(int[] arr,int i ,int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+    /**
+     * 0 ~ n-1 、1 ~ n-1 、2 ~ n-1 、3 ~ n-1   由外层循环控制  i 从 0 开始    i++
+     * 内层循环，从i之后的位置开始比较，找到最小值的位置。
+     * @param arr
+     */
     public static void selectionSort(int[] arr){
         if (arr == null|| arr.length < 2){
             return;
@@ -50,21 +77,42 @@ public class Select_Bobble_InsertionSort {
         }
     }
 
+    /**
+     * 冒泡排序
+     * 0 ~ n-1  范围
+     * 0 ~ n-2  范围
+     * 0 ~ n-3  范围
+     * 0 ~ n-4  范围
+     *
+     * 外层循环控制end范围，注意 end可以为0，但没必要，因为当只剩下0 和 1 两个元素后 （end = 1）比较完成后只剩下一个数（end=0），没有意义再进行一排序了
+     * 内层循环 second 表示比较的第二位元素 和second -1 前一位元素进行比较
+     * 外层循环从0开始，内层循环second表示第二位元素，所以second = 1
+     * 注意 second 可以到达最后一位 end 所以 second <= end
+     * @param arr
+     */
     public static void bubbleSort(int[] arr){
         if (arr == null|| arr.length < 2){
             return;
         }
         int N = arr.length;
         for (int end = N-1; end > 0 ; end--) {
-            for (int i = 0; i < end; i++) {
-                if (arr[i] > arr[i+1] ){
-                    swap(arr,i,i+1);
+            for (int second = 1; second <= end; second++) {
+                if (arr[second] < arr[second-1] ){
+                    swap(arr,second,second-1);
                 }
             }
         }
     }
 
-
+    /**
+     * 插入排序
+     * 0 ~ n-1 排序
+     * 1 ~ n-1 排序
+     * 2 ~ n-1 排序
+     * 3 ~ n-1 排序
+     * 外层循环 i 从 0 开始(或者直接从1开始比较)，i ++  到n-1为止
+     * @param arr
+     */
     public static void insertionSort(int[] arr){
         if (arr == null|| arr.length < 2){
             return;
@@ -102,8 +150,8 @@ public class Select_Bobble_InsertionSort {
             int[] arr = generateRandomArr(20, 10);
             int[] copy = copy(arr);
 //            selectionSort(copy);
-//            bubbleSort(copy);
-            insertionSort(copy);
+            bubbleSort(copy);
+//            insertionSort(copy);
             if(!checkSort(arr,copy)){
                 System.out.println("排序失败！");
                 printArr(arr);
