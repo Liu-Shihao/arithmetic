f0e70e7f1a435a8b6feff57233c0f1ee23dde527
LiuShihao <1692312138@qq.com>
2022-02-22 16:09:51 +0800

    归并排序-非递归方法

--- src/main/java/com/lsh/day02_sort/Code04_MergeSort.java
+++ src/main/java/com/lsh/day02_sort/Code04_MergeSort.java
@@ -1,5 +1,7 @@
 package com.lsh.day02_sort;
 
+import java.util.Arrays;
+
 /**
  * @author ：LiuShihao
  * @date ：Created in 2022/2/11 2:51 下午
@@ -12,18 +14,29 @@ package com.lsh.day02_sort;
  * 3. arr[M+1..R]范围上有序
  * 4. merge 合并(借助两个指针，一个数组)
  *
+ * T(N) = 2 * T(N/2) + O(N)  ; a = 2 b = 2 d = 1
+ * O(N * logN)
+ *
  */
 public class Code04_MergeSort {
 
     public static void main(String[] args) {
-        int testTime = 100;
+        System.out.println("测试开始");
+        int testTime = 10000;
         for (int i = 0; i < testTime; i++) {
             int[] arr = SortUtil.generateRandomArray(10, 20);
-            SortUtil.printArr(arr);
-            mergeSort1(arr);
-            SortUtil.printArr(arr);
-            System.out.println("====================");
+            int[] copyArray = SortUtil.copyArray(arr);
+            Arrays.sort(arr);
+//            mergeSort1(copyArray);
+            mergeSort2(copyArray);
+            if (!SortUtil.isEqual(arr,copyArray)){
+                System.out.println("出错了！");
+                SortUtil.printArr(arr);
+                SortUtil.printArr(copyArray);
+                break;
+            }
         }
+        System.out.println("测试结束");
     }
     /**
      * 递归方法实现归并排序
@@ -34,6 +47,7 @@ public class Code04_MergeSort {
         if (arr == null || arr.length < 2){
             return ;
         }
+        //让arr[0...N-1]范围上有序
         process(arr,0,arr.length-1);
     }
 
@@ -44,14 +58,17 @@ public class Code04_MergeSort {
      * @param R
      */
     public static void process(int[] arr,int L,int R){
-        //base case
+        //base case 如果L == R表示：只有一个元素，一个元素即是有序的
         if (L == R){
             return;
         }
+        //不止一个元素，取中点，二分，
         //mid = (L + R)/2;  使用L + ((L - R) >> 1);更安全，如果L+R下表位置很大，则会越界
         int mid = L + ((R - L) >> 1);
+        //再分别将左右两部分的数组进行排序 arr[L..mid]、arr[mid+1...R]
         process(arr,L,mid);
         process(arr,mid+1,R);
+        //左边右边有序后，进行合并，使用两个指针，那个小排谁，其指针后移
         merge(arr,L,mid,R);
 
     }
@@ -68,7 +85,7 @@ public class Code04_MergeSort {
         int i = 0 ;
         int p1 = L;
         int p2 = M + 1;
-        //p1 和 p2都没有越界
+        //p1 和 p2 都没有越界
         while (p1 <= M && p2 <= R){
             help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
         }
@@ -94,9 +111,34 @@ public class Code04_MergeSort {
         if (arr == null || arr.length < 2){
             return ;
         }
+        int N = arr.length;
+        // 步长 mergeSize  当前有序的，左组长度
         int step = 1;
-        while (step < arr.length){
-
+        //步长小于数组长度
+        while (step < N){
+            // L 当前左组的第一个位置
+            int L = 0;
+            //当左组开始位置大于数组长度时，停止
+            while (L < N){
+                // L ..步长.. M  左组  ；M 表示完整左组的最后一个位置
+                int M = L + step - 1;
+                //如果左组的最后一个位置 超过了 数组的长度：说明没有凑够一个左组，
+                if (M >= N){
+                    break;
+                }
+                //定义右边界，正常的R位置应该是M+step，但是不能超过数组长度，所以两者取最小值
+                int R = Math.min(M+step,N-1);
+                //进行合并
+                merge(arr,L,M,R);
+                //下一组左组的第一个位置
+                L = R + 1;
+            }
+            //防止溢出
+            if (step > N/2){
+                break;
+            }
+            //步长x2
+            step <<= 1;
         }
     }
 
